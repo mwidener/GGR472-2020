1f6aba811ca46038711e95e9707451f58c07687a
mwidener <mwidener@gmail.com>
2020-01-16 10:27:54 -0500

    test update

---
+++ content/lecture2/lec2_2/_index.md
@@ -0,0 +1,10 @@
+---
+title: "Strengths/Weaknesses of Web Maps"
+date: 2020-01-03T15:16:16-05:00
+chapter: true
+weight: 2
+---
+
+# Strengths of Web Maps
+
+# Weaknesses of Web Maps
--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 hugo -t hugo-theme-learn # if using a theme replace with 'hugo -t <YOURTHEME>'
 
 # Go to Public folder
-cd public
+cd public @ b3e6af1 
 
 # Add changes to git.
 git add .
